f2b33c199325edb484dd7444b9f57b74d62bcd7e
Sai Phaninder Reddy Jonnala <sai.jonnala@gopassport.com>
2016-12-23 16:12:38 -0500

    testing endpoints

--- src/main/java/HelloSpark.java
+++ src/main/java/HelloSpark.java
@@ -4,6 +4,15 @@ import static spark.Spark.*;
 public class HelloSpark {
 	public static void main(String[] args) {
 		port(9090);
-		get("/hello", (req, res) -> "Hello World");
+
+		get("/", (request, response) -> "Root GET");
+		post("/", (request, response) -> {
+			return "Root POST";
+		});
+
+		get("/hello/:name", (request, response) -> {
+			return "Hello Mr." + request.params(":name");
+		});
+
 	}
 }
