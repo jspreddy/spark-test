baec9f26d9c14f33509eb1af99f8c03739d3be51
Sai Phaninder Reddy Jonnala <sai.jonnala@gopassport.com>
2017-04-05 09:21:27 -0400

    dependencies:Gson,Velocity + object + json transformer

--- src/main/java/HelloSpark.java
+++ src/main/java/HelloSpark.java
@@ -2,17 +2,34 @@
 import static spark.Spark.*;
 
 public class HelloSpark {
+
+	private HelloSpark(){}
+
+	public static class Test{
+		public String request_data="";
+		public Test(String data){
+			this.request_data = data;
+		}
+	}
+
 	public static void main(String[] args) {
 		port(9090);
 
-		get("/", (request, response) -> "Root GET");
+		get("/", (request, response) -> {
+			return new Test("Root GET");
+		}, new JsonTransformer());
+
 		post("/", (request, response) -> {
-			return "Root POST";
-		});
+			return new Test("Root POST");
+		}, new JsonTransformer());
 
 		get("/hello/:name", (request, response) -> {
-			return "Hello Mr." + request.params(":name");
-		});
+			return new Test("Hello Mr." + request.params(":name"));
+		}, new JsonTransformer());
+
+		post("/hello/:name", (request, response) -> {
+			return new Test("Hello Mr." + request.params(":name"));
+		}, new JsonTransformer());
 
 	}
 }
